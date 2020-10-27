d051b14595d6bc77923a255ce9b8ac6ef3dcf30f
Aaron Ray <aaray619@gmail.com>
2020-10-27 18:40:32 -0500

    added more inquirer code

--- server.js
+++ server.js
@@ -14,3 +14,31 @@ var connection = mysql.createConnection({
     password: "password",
     database: "emp_mang_db"
   });
+  connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+  });
+
+  inquirer.prompy([
+    {
+      type: "list",
+      name: "actionChoice",
+      message: "What do you want to view?",
+      choices: ["Roles", "Departments", "Employees"],
+      default: "Employees"
+    }
+  ]).then(function (res) {
+    if (res.actionChoice === "Roles"){
+      inquirer.prompt([{
+        type: "list",
+        name: "action",
+        message: "What do you want to do",
+        choices: ["Add Role", "Delete Role", "View Roles"],
+        default: "Employees"
+      }]).then(function (res){
+        if (res.action === "Add Role"){
+          
+        }
+      })
+    }
+  })
