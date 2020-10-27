20fd968e6d9ca07d8d0e996d53113426e3b6cfbf
Aaron Ray <aaray619@gmail.com>
2020-10-27 18:10:00 -0500

    added code to empMan sql

--- sql/employyeMang.sql
+++ sql/employyeMang.sql
@@ -1,3 +1,27 @@
-CREATE database top_songsDB;
+CREATE database emp_mang_db;
 
-USE top_songsDB;
+USE emp_mang_db;
+
+CREATE TABLE department (
+
+    id INTEGER(2) AUTO_INCREMENT primary key,
+    name VARCHAR(30)
+
+);
+CREATE TABLE role (
+    id INTEGER(2) AUTO_INCREMENT  primary key,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INTEGER(2),
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
+CREATE TABLE employee (
+    id INTEGER(2) AUTO_INCREMENT  primary key,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INTEGER(2),
+    manager_id INTEGER(2),
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
+
+);
